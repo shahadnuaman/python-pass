bd325c86c5f2c9add651492876095bafdb989c28
Layth Zahid <laith43d@gmail.com>
2021-09-24 02:28:44 +0300

    Pass task

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # python-pass
+
+1. Fork the repo
+2. Clone the repo to your machine
+3. resolve the pass task
+   * Please refer to python_pass.py for instructions
+4. Commit and push your work to github
+5. Generate a pull request
+6. Cross fingers and wait!
+
+Good luck to all participants..
+
--- python_pass.py
+++ python_pass.py
@@ -1,15 +1,14 @@
+"""
+Instructions:
+
+1. Create a class named ReversedString that inherits from StringOperations class
+2. Implement the function reverse
+3. reverse function should be a one liner function that returns the reverse string to_be_reversed
+4. Instantiate the class ReversedString
+5. Print to show your function implementation result
+"""
 
 
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
-
-
-class ReversedString(StringOperations):
-    def reverse(self, *, to_be_reversed: str = None):
-        return to_be_reversed[::-1]
-
-
-var = ReversedString()
-
-print(var.reverse(to_be_reversed='Layth'))
